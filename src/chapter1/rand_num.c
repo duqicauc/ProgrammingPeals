7c5a4559c011b4e939a87d41b21cbf5bcf837234
duqi <duqi_2009@163.com>
2013-10-31 20:50:44 +0800

    生成[0,10000000]之间的1000000个整数，注意函数栈空间可能不够用，所以数组定义为全局数组才够用

--- src/chapter1/rand_num.c
+++ src/chapter1/rand_num.c
@@ -4,38 +4,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
 
-//#define ARRAY_NUMBER 10000000
-//#define SORT_NUMBER 1000000
+int num[10000000];
 unsigned bigrand();
 unsigned randint(int l, int u);
-void    swap(int *a,int *b);
+void     swap(int *a,int *b);
 
 int main(int argc,char* argv[])
 {
-    int num[1000000];
     int i;
-    FILE *rand_num_file = fopen("rand_num_file.txt","w+");
-   
+    FILE *fp = fopen("rand_num_file.txt","a+");
+    if(fp == NULL)
+    {
+        printf("cannot open this file!\n");
+        exit(0);
+    }
     srand((unsigned)time(NULL));
     
-    for( i=0; i<1000000; i++)
-        num[i] = randint(1000000,9999999);
-         
-    for( i=0; i<1000000; i++)
-        fprintf(rand_num_file,"%d\n",num[i]);
-   
+    for(i=0; i<10000000; i++)
+        num[i] = i;
+    for(i=0; i<1000000; i++)
+    {
+        swap(&num[i],&num[randint(1000000,10000000-1)]);
+        fprintf(fp,"%d\n",num[i]);
+    }
+    fclose(fp);
     return 0;
 }
 
-/*实现randint(l,u)，返回[l,u]范围内的一个随机整数
-int randint(int l,int u)
-{
-    int temp;
-    temp = floor(l + (1.0*rand()/RAND_MAX)*(u - l + 1 ));
-    return temp;
-}*/
 /* 
  * 生成大随机数（30位）
  **/
@@ -49,7 +45,7 @@ unsigned int bigrand()
  **/
 unsigned int randint(int l, int u)
 {
-        return l + bigrand() % (u-l+1);
+    return l + bigrand() % (u-l+1);
 }
 
 //交换
