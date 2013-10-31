762aa0188cbe280f7056618a0aa4aaaf8abb3882
duqi <duqi_2009@163.com>
2013-10-31 22:30:37 +0800

    申请大的内存空间：数组或者打开大文件时候，不要用普通的局部变量，函数栈空间不够用，要声明为全局变量或者静态变量

--- src/chapter1/qsort.c
+++ src/chapter1/qsort.c
@@ -7,17 +7,21 @@ int intcomp(void *a,void *b)
 }
 
 int num[1000000];
+FILE *file;
+FILE *result_file;
 
 int main(int argc,char* argv[])
 {
-    FILE *file = fopen("rand_num_file.txt", "r+");
-    FILE *result_file = fopen("result.txt","w+");
-    int i;
-    int n=0;
+    file = fopen("rand_num_file.txt","r+");
+    result_file = fopen("result.txt","w+");
+    int i,n=0;
     while(fscanf(file,"%d",&num[n]) != EOF)
         n++;
+    
     qsort(num,1000000,sizeof(num[0]),(__compar_fn_t)intcomp);
+   
     for(i=0;i<1000000;i++)
         fprintf(result_file,"%d\n",num[i]);
+    
     return 0;
 }
