f94fea2be1c875f1cb95e0ec2ae6845b265239c9
杜琪 <duqi_2009@163.com>
2013-11-07 18:42:01 +0800

    格式化信函生成器可以工作了，哦也
    (1)字符串是不能改变的，替换之后必须将之重新赋值给新的变量
    (2)Python从元组中取出来的时候是无类型的，但replace要求的参数是string的，所以要用str转换一下

--- src/generator.py
+++ src/generator.py
@@ -6,8 +6,9 @@ def generator(templateText):
     yuansu = ('$1','$2','$3','$4')
     student1 = {'$1':'Zed','$2':'c language','$3':36,'$4':'The teaching building 3'}
     for i in yuansu:
-        print i
-        templateText.replace(str(i),student1[str(i)])
+        old = str(i)
+        new = str(student1[i])
+        templateText = templateText.replace(old,new)
     return templateText
 
 print generator(templateText)
