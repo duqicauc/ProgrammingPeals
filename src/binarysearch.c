f6d31f8956412c9c7cbb6547010f05b1ce48eb62
杜琪 <duqi_2009@163.com>
2013-11-10 13:20:47 +0800

    添加自动化测试的相关代码~
    (1)测试函数的主循环是，数组里的有效元素个数n从0变化到最大合理值
    (2)第一部分测试所有元素互异的情况
    (3)第二部分测试所有元素相同的情况
    (3)测试搜索算法要测试三个区间

--- src/binarysearch.c
+++ src/binarysearch.c
@@ -29,11 +29,42 @@ int binarysearch(DataType t)
 /* C语言脚手架：调用binarysearch的驱动程序 */
 int main()
 {
-	int i,t;
-	while (scanf("%d %d",&n,&t) != EOF){
-		for(i = 0; i < n; i++)
-			x[i] =  10*i;
-		printf(" %d\n",binarysearch(t));
+        int i,t,maxn = 1000;
+
+//		/* 人工测试 */
+//        while (scanf("%d %d",&n,&t) != EOF){
+//                for(i = 0; i < n; i++)
+//                        x[i] =  10*i;
+//                printf(" %d\n",binarysearch(t));
+//        }
+
+	/* 自动化测试 */
+	/* test distinct elements(plus one at the end) */
+	for (n=0; n<=maxn; n++) {// 测试主循环
+		printf("n=%d\n",n);//当前测试数组中的元素个数
+			
+		/* 测试所有元素互异的情况*/
+		for (i=0; i<=n; i++) {				
+			x[i] = 10 * i;
+		}
+		for (i=0; i<n; i++) {
+			assert(binarysearch(10*i) == i);
+			assert(binarysearch(10*i - 5) == -1);
+		}
+		assert(binarysearch(10*n - 5) == -1);
+		assert(binarysearch(10*n) == -1);
+
+		/* test equal elements */
+		for (i=0; i<=n; i++) {
+			x[i] = 10;
+		}
+		if (n == 0){
+			assert(binarysearch(10) == -1);
+		}
+		else{
+			assert(binarysearch(0<=binarysearch(10) && binarysearch(10)<n));
+		}
+		assert(binarysearch(5) == -1);
+		assert(binarysearch(15) == -1);
 	}
-	return 0;
 }
